dc3ea038f5d5b23e92b17af253796b0e22776b9c
martazh <102956419+martazh@users.noreply.github.com>
2022-05-24 16:51:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const starWrapper = document.querySelector(".stars");
+
+
+const stars = document.querySelectorAll(".stars a");
+
+stars.forEach((star, clickedIdx) => {
+    star.addEventListener("click", () => {
+        starWrapper.classList.add("disabled")
+        stars.forEach((otherStar, otherIdx) => {
+            if (otherIdx <= clickedIdx) {
+                otherStar.classList.add("active");
+
+            }
+        });
+        console.log(`Rating of ${clickedIdx +1} star was clicked`);
+    });
+});
